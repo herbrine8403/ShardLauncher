e5b590cc7f54b96cdef37caadefefbe98e89f7b1
herbrine8403 <weishixvn@outlook.com>
2026-03-23 20:30:53 +0800

    fix: 🔧添加PathManager缺少的DIR_COMPONENTS等字段

--- ShardLauncher/src/main/java/com/lanrhyme/shardlauncher/game/path/PathManager.kt
+++ ShardLauncher/src/main/java/com/lanrhyme/shardlauncher/game/path/PathManager.kt
@@ -7,19 +7,27 @@ object PathManager {
     lateinit var DIR_FILES_PRIVATE: File
     lateinit var DIR_FILES_EXTERNAL: File
     lateinit var DIR_CACHE: File
+    lateinit var DIR_NATIVE_LIB: String
     
     lateinit var DIR_GAME: File
     lateinit var DIR_ACCOUNT_SKIN: File
     lateinit var DIR_LAUNCHER_LOGS: File
+    lateinit var DIR_COMPONENTS: File
+    
+    lateinit var FILE_MINECRAFT_VERSIONS: File
 
     fun refreshPaths(context: Context) {
         DIR_FILES_PRIVATE = context.filesDir
         DIR_FILES_EXTERNAL = context.getExternalFilesDir(null) ?: context.filesDir
         DIR_CACHE = context.cacheDir
+        DIR_NATIVE_LIB = context.applicationInfo.nativeLibraryDir
 
         DIR_GAME = File(DIR_FILES_PRIVATE, "games")
         DIR_ACCOUNT_SKIN = File(DIR_GAME, "account_skins")
         DIR_LAUNCHER_LOGS = File(DIR_FILES_EXTERNAL, "logs")
+        DIR_COMPONENTS = File(DIR_FILES_PRIVATE, "components")
+        
+        FILE_MINECRAFT_VERSIONS = File(DIR_GAME, "minecraft_versions.json")
 
         createDirs()
     }
@@ -28,5 +36,6 @@ object PathManager {
         DIR_GAME.mkdirs()
         DIR_ACCOUNT_SKIN.mkdirs()
         DIR_LAUNCHER_LOGS.mkdirs()
+        DIR_COMPONENTS.mkdirs()
     }
 }
